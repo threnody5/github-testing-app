e729f5b83254f1a6ca5c71a4367e266291ef7362
Tina Youssefian <tyousofine@gmail.com>
2023-01-11 13:03:19 -0500

    add tina component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 /** @format */
 
 import './App.css';
+import Tina from './tina/tina';
 
 function App() {
   return (
     <div>
       <div>Testing Main Branch Protections</div>
       <div>Second Test</div>;
+      <Tina />
+
+
     </div>
   );
 }
